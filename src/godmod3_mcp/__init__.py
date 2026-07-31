dd8b53e04e19e83f72e61fcbd7564e1624670acd
Vudu Ronin <m1ivancevic@gmail.com>
2026-07-31 03:13:18 +0200

    fix: remove main from __all__ in __init__.py for clarity

--- src/godmod3_mcp/__init__.py
+++ src/godmod3_mcp/__init__.py
@@ -2,7 +2,6 @@
 
 from .client import Godmod3Client, Godmod3ClientError
 from .config import Config
-from .server import main
 
-__all__ = ["Config", "Godmod3Client", "Godmod3ClientError", "main"]
+__all__ = ["Config", "Godmod3Client", "Godmod3ClientError"]
 __version__ = "0.1.0"
